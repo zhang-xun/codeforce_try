8c1c299929a14e9d059cf8ad8a45528c6094dcc8
zhangxun <15025689012@163.com>
2017-02-28 10:41:41 +0800

    commit in 2.28

---
+++ 739A_AlyonaAndMex.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main():
+    n,m = input().split()
+    maxminmex = int(n)
+    for item  in range(int(m)):
+        li, ri = input().split()
+        maxminmex = min(maxminmex,int(ri)-int(li)+1)
+       
+    print(maxminmex)
+
+    for i in range(int(n)):
+        print(i % maxminmex)
+
+if __name__ == "__main__":
+    main()
